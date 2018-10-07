b88af38b8fffef2a75d8ccf108a5bdec7f481058
screetBloom <15306225869@163.com>
2018-10-07 21:00:33 +0800

    done

---
+++ config.js
@@ -0,0 +1,29 @@
+
+const A = {
+    name: ``,
+    age: ``,
+    foods: [],
+    child: {
+        super: '',
+        name: '',
+        age: ``,
+        foods: [],
+        children: {
+            super: '',
+            name: '',
+            age: ``,
+            foods: [],
+        }
+    },
+    test: ['', '', {
+        name: '',
+        children: {
+            name: '',
+            test: []
+        }
+    }],
+};
+
+window.target = {
+    A
+};
